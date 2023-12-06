56f0b9427e672ac7dc08064cae1bb78fea5b6b41
shani <shani.valensi1@gmail.com>
2023-12-06 09:35:07 +0200

    operation functions

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 def show_dashboard():
-    dashboard="Calculator:\n '+':addition \n '-':substraction \n '*':multiplication \n '/':division \n '0':close"
+    dashboard = "Calculator:\n '+':addition \n '-':substraction \n '*':multiplication \n '/':division \n '0':close"
     print(dashboard)
     action = input("enter your desired action: ")
 
-def addition(n1,n2):
-    print(n1+n2)
-show_dashboard()
 
+def addition(n1, n2):
+    print(n1 + n2)
+
+
+def subtraction(n1, n2):
+    print(n1 - n2)
+
+
+def multiplication(n1, n2):
+    print(n1 * n2)
+
+
+def division(n1, n2):
+    print(n1 / n2)
+
+
+show_dashboard()
