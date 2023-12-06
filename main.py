b82649b47009276d36e3dad7e92004091b22e922
shani <shani.valensi1@gmail.com>
2023-12-06 09:17:36 +0200

    f

---
+++ main.py
@@ -0,0 +1,7 @@
+def show_dashboard():
+    dashboard="Calculator:\n '+':addition \n '-':substraction \n '*':multiplication \n '/':division \n '0':close"
+    print(dashboard)
+    action = input("enter your desired action: ")
+    
+show_dashboard()
+
