b824e1d766254f0560e7ee064c17e5ed85a01f33
shani <shani.valensi1@gmail.com>
2023-12-06 09:29:22 +0200

    test

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ def show_dashboard():
     dashboard="Calculator:\n '+':addition \n '-':substraction \n '*':multiplication \n '/':division \n '0':close"
     print(dashboard)
     action = input("enter your desired action: ")
-    
+
+def addition(n1,n2):
+    print(n1+n2)
 show_dashboard()
 
